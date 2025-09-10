bbfc446b070e66ecee6864a99455113aec76de37
hidehic0 <hidehico.ooba@gmail.com>
2025-09-10 10:04:55 +0900

    引数をパースするコードを追加

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(name = env!("CARGO_PKG_NAME"),version = env!("CARGO_PKG_VERSION"),arg_required_else_help = true,)]
+struct Args {
+    problemid: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("The selected problem is {}", args.problemid)
+}
